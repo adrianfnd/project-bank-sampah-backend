2bb96f9b5590b0f22722d5e7393200f848310244
adrianfnd <fernanda.adrian2@gmail.com>
2024-04-30 21:55:45 +0700

    create roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('id_role');
+            $table->foreign('id_role')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
         });
