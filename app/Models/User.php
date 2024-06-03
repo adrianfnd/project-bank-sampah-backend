429f669a925508070fee806266d5215e570177f4
adrianfnd <fernanda.adrian2@gmail.com>
2024-06-03 11:47:05 +0700

    rev users image

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'address',
         'email',
         'phone_number',
+        'image',
         'otp',
         'email_verified_at',
         'password',
